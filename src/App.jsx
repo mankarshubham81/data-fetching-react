7d7a02cfe7c4b897412a9f0e7f75534bb9456d0d
Shubham Mankar <mankarshubham81@gmail.com>
2024-04-28 09:03:08 +0530

    removed un necessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import FetchMethod from './components/FetchMethod'
 import AsyncAwait from './components/AsyncAwait'
